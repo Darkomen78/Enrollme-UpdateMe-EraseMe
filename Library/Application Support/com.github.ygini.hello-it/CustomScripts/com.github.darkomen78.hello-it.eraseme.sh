44c2a9b1f52b055deb3ea943c1d89e27ffe2e547
Sylvain <darkomen@me.com>
2021-11-24 12:36:05 +0100

    Switch to DEPnotify and use full erase-install pkg

--- Library/Application Support/com.github.ygini.hello-it/CustomScripts/com.github.darkomen78.hello-it.eraseme.sh
+++ Library/Application Support/com.github.ygini.hello-it/CustomScripts/com.github.darkomen78.hello-it.eraseme.sh
@@ -6,10 +6,8 @@ logfile=~/Library/Logs/eraseme.log
 
 function onClickAction {
 date >> $logfile
-sleep 1
-open $logfile
-sleep 1
-sudo /Users/Shared/IT/erase/erase-install.sh --erase --update --confirm --extras="/Users/Shared/IT/erase/pkg" >> $logfile 2>&1 & disown
+# open $logfile
+sudo /Library/Management/erase-install/erase-install.sh --check-power --update  --erase --confirm --extras="/Users/Shared/IT/erase/pkg" --depnotify >> $logfile 2>&1 & disown
 }
 
 main $@
--- Library/Application Support/com.github.ygini.hello-it/CustomScripts/com.github.darkomen78.hello-it.updateme.sh
+++ Library/Application Support/com.github.ygini.hello-it/CustomScripts/com.github.darkomen78.hello-it.updateme.sh
@@ -8,11 +8,9 @@ function onClickAction {
 USRSHORTNAME="$(ls -la /dev/console | awk '{print $3}')"
 HelloIT_item="/Users/${USRSHORTNAME}/Library/Preferences/com.github.ygini.Hello-IT.updateme.plist"
 date >> $logfile
-sleep 1
-open $logfile
-[[ -f ${HelloIT_item} ]] && rm ${HelloIT_item} ;
-sleep 2 ;
-sudo /Users/Shared/IT/erase/erase-install.sh --check-power --update --reinstall >> $logfile 2>&1 & disown
+# open $logfile
+[[ -f ${HelloIT_item} ]] && rm ${HelloIT_item}
+sudo /Library/Management/erase-install/erase-install.sh --check-power --update --reinstall --depnotify >> $logfile 2>&1 & disown
 }
 
 main $@
